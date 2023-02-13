2711e163bf1650b72251e639ff828605af6c1b38
hyunjine <thevlakk1@gmail.com>
2023-02-13 17:50:34 +0900

    치킨 쿠폰

--- 로그인 성공.py
+++
@@ -1,9 +0,0 @@
-def solution(id_pw, db):
-    answer = ''
-    arr = [i for i in db if i[0] == id_pw[0]]
-    if not arr:
-        return "fail"
-    arr2 = [i for i in arr if i[1] == id_pw[1]]
-    if not arr2:
-        return "wrong pw"
-    return "login"
